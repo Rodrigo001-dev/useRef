70ab499f342cdf78329dde5f656b26097a85bae5
Rodrigo Rael <rodrigorael53@gmal.com>
2020-09-10 15:42:48 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useCallback, FormEvent } from 'react';
+
+import Input from './components/input';
+import Modal, { ModalHandles } from './components/Modal';
+
+
+
+function App() {
+  const nameInputRef = useRef<HTMLInputElement>(null);
+  const acceptTermsRef = useRef({ value: false });
+  const modalRef = useRef<ModalHandles>(null);
+
+  const handleSubmit = useCallback((e: FormEvent) => {
+    e.preventDefault();
+
+    console.log(nameInputRef.current?.value);
+    console.log(acceptTermsRef.current.value);
+  }, []);
+
+  const handleAcceptTerms = useCallback(() => {
+    acceptTermsRef.current.value = !acceptTermsRef.current.value;
+  }, []);
+  
+  const handleOpenModal = useCallback(() => {
+    modalRef.current?.openModal();
+  }, []);
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <Input 
+          name="name" 
+          label="Nome completo" 
+          ref={nameInputRef}
+        />
+
+        <button type="button" onClick={handleAcceptTerms}>
+          Aceitar termos
+        </button>
+
+        <button type="submit">Enviar</button>
+      </form>
+
+      <button onClick={handleOpenModal}>Abrir modal</button>
+
+      <Modal ref={modalRef} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Modal.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useCallback, forwardRef, useImperativeHandle } from 'react';
+
+export interface ModalHandles {
+  openModal: () => void;
+}
+
+const Modal: React.RefForwardingComponent<ModalHandles> = (props, ref) => {
+  const [visible, setVisible] = useState(true);
+
+  const openModal = useCallback(() => {
+    setVisible(true);
+  }, []);
+
+  useImperativeHandle(ref, () => {
+    return {
+      openModal
+    };
+  });
+
+  const handleCloseModal = useCallback(() => {
+    setVisible(false);
+  }, []);
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <div>
+      Modal aberto
+
+      <button onClick={handleCloseModal}>Fechar modal</button>
+    </div>
+  );
+}
+
+export default forwardRef(Modal);
